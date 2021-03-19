fd3d8405babc464ca4db9d35a91509aba40d6f00
Ian <ian@ipoesse.de>
2021-03-19 23:28:21 +0100

    add gitignore and remove build files

--- middleware/Program.cs
+++ middleware/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using NLog;
 
 namespace middleware
 {
     class Program
     {
+        private static Logger log = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
