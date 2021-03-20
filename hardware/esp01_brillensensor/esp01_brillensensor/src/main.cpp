a8ef009b10c9d1f7b4beb19960e77b1ddd010c6b
Snake4you <info@snake4you.de>
2021-03-20 04:22:07 +0100

    Script ESP01 für Brille angelegt

---
+++ hardware/esp01_brillensensor/esp01_brillensensor/src/main.cpp
@@ -0,0 +1,122 @@
+
+#include "MPU9250.h"
+#include <Arduino.h>
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <MQTT.h>
+
+
+const char ssid[] = "ssid";
+const char pass[] = "pass";
+
+
+WiFiClient net;
+MQTTClient client;
+
+MPU9250 IMU(Wire,0x68);
+
+int status;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "public", "public")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/hello");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
+}
+
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, pass);
+
+  client.begin("public.cloud.shiftr.io", net);
+  client.onMessage(messageReceived);
+
+  connect();
+
+  Wire.begin(0,2);
+  // serial to display data
+
+  while(!Serial) {}
+
+  // start communication with IMU 
+  status = IMU.begin();
+  if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while(1) {}
+  }
+  // setting the accelerometer full scale range to +/-8G 
+  IMU.setAccelRange(MPU9250::ACCEL_RANGE_4G);
+  // setting the gyroscope full scale range to +/-500 deg/s
+  IMU.setGyroRange(MPU9250::GYRO_RANGE_250DPS);
+  // setting DLPF bandwidth to 20 Hz
+  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
+  // setting SRD to 19 for a 50 Hz update rate
+  IMU.setSrd(19);
+}
+
+void loop() {
+
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 200) {
+    lastMillis = millis();
+    IMU.readSensor(); // read the sensor
+
+    // display the data
+    Serial.print(IMU.getAccelX_mss(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getAccelY_mss(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getAccelZ_mss(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroX_rads(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroY_rads(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroZ_rads(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagX_uT(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagY_uT(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagZ_uT(),6);
+    Serial.print("\t");
+    Serial.println(IMU.getTemperature_C(),6);
+    //client.publish("/hello", "world");
+  }
+
+}
