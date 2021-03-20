ed6f485d073e01e6d5b79ada9182ba6ec8b0b414
unknown <kobkrebs@gmail.com>
2021-03-20 14:44:51 +0100

    implemented a second Panel and connected all current Buttons

--- GUI/Assets/Scripts/SideMenuV.cs
+++ GUI/Assets/Scripts/SideMenuV.cs
@@ -7,24 +7,37 @@ using UnityEngine.UI;
 public class SideMenuV : MonoBehaviour
 {
 
-    public GameObject Background;
-    public GameObject BackgroundFollow;
+    public GameObject StartScreen;
+    public GameObject PflegeScreen;
 
     Color lerpedColor = Color.white;
 
-    Color green = new Color(0.0177f, 0.5377f, 0.0697f);
-    Color yellow = new Color(0.7924f, 0.7423f, 0.0261f);
-    Color lightblue = new Color(0.3434f, 0.7047f, 0.7830f);
-    Color red = new Color(0.6117f, 0.21157f, 0.1411f);  
-    Color black = new Color(0.2264151f, 0.2264151f, 0.2264151f);
+    Color colorCommunication = new Color(0.0177f, 0.5377f, 0.0697f);
+    Color colorEntertainment = new Color(0.7924f, 0.7423f, 0.0261f);
+    Color colorTraining = new Color(0.3434f, 0.7047f, 0.7830f);
+    Color colorHelp = new Color(0.6117f, 0.21157f, 0.1411f);  
+    Color colorStartScreen = new Color(0.2264151f, 0.2264151f, 0.2264151f);
+
+    Color startColor;
+    Color endColor;
+    float colorTime;
+    float duration;
 
     public GameObject lastSelectedButton;
+    public int currentPanel;
 
     //Buttons
+    //Startpanel
     public GameObject Communication_Button;
     public GameObject Entertainment_Button;
     public GameObject Training_Button;
     public GameObject Help_Button;
+
+    //Pflegepanel
+    public GameObject LeaveHelp_Button;
+    public GameObject Toilet_Button;
+    public GameObject Pain_Button;
+    public GameObject OtherMed_Button;
 
 
 
@@ -33,53 +46,118 @@ public class SideMenuV : MonoBehaviour
     {
         //Communication_Button.GetComponent<Button>().colors.normalColor = new Color(0.5f, 0.5f, 0.5f);
         highlightButton(lastSelectedButton);
+
+        startColor = colorStartScreen;
+        endColor = colorStartScreen;
+        duration = 1.3f;
     }
 
 
     void Update()
     {
+        {
+            lerpedColor = Color.Lerp(startColor, endColor, colorTime);
+            StartScreen.GetComponent<Image>().color = lerpedColor;
+            PflegeScreen.GetComponent<Image>().color = lerpedColor;
+            if (colorTime < 1){ // while t below the end limit...
+                // increment it at the desired rate every update:
+                colorTime += Time.deltaTime/duration;
+            }else if(colorTime == 1){
+                startColor = endColor;
+            }
+        }
+
+        void recolorAllBackgrounds(Color destinationColor){
+            endColor = destinationColor;
+            colorTime = 0;
+        }
+
+
         /*lerpedColor = Color.Lerp(Color.black, green, Mathf.PingPong(Time.time, 1));
         //Debug.Log(lerpedColor);
         Background.GetComponent<Image>().color=lerpedColor;
         BackgroundFollow.GetComponent<Image>().color=lerpedColor;
         //Debug.Log(Background.GetComponent<Image>().color);*/
 
-        Debug.Log("Penis");
-        Debug.Log(Communication_Button.GetComponent<Button>().colors.normalColor);
+        //Debug.Log("Penis");
+        //Debug.Log(Communication_Button.GetComponent<Button>().colors.normalColor);
 
         //reaction on inputs my guy
         if (Input.GetKeyUp(KeyCode.Keypad8))
         {
             Debug.Log("Up.");
-            switch(lastSelectedButton.name){
-                case "Button_Unterhaltung":
-                    changeButton(Communication_Button);
-                    break;
-                case "Button_Training":
-                    changeButton(Entertainment_Button);
-                    break;
-                case "Button_Pflege":
-                    changeButton(Training_Button);
-                    break;
+            if(currentPanel == 1){
+                switch(lastSelectedButton.name){
+                    case "Entertainment_Button":
+                        changeButton(Communication_Button);
+                        break;
+                    case "Training_Button":
+                        changeButton(Entertainment_Button);
+                        break;
+                    case "Help_Button":
+                        changeButton(Training_Button);
+                        break;
+                }
+            }else if(currentPanel == 2){
+                
+                switch(lastSelectedButton.name){
+                    case "Toilet_Button":
+                        changeButton(LeaveHelp_Button);
+                        break;
+                    case "Pain_Button":
+                        changeButton(Toilet_Button);
+                        break;
+                    case "OtherMed_Button":
+                        changeButton(Pain_Button);
+                        break;
+                }
             }
         }
         if (Input.GetKeyUp(KeyCode.Keypad5))
         {
             Debug.Log("confirm.");
+            switch(lastSelectedButton.name){
+                case "Help_Button":
+                    PflegeScreen.SetActive(true);
+                    recolorAllBackgrounds(colorHelp);
+                    changeButton(LeaveHelp_Button);
+                    currentPanel = 2;
+                    break;
+                case "LeaveHelp_Button":
+                    PflegeScreen.SetActive(false);
+                    recolorAllBackgrounds(colorStartScreen);
+                    changeButton(Communication_Button);
+                    currentPanel = 1;
+                    break;
+            }
         }
         if (Input.GetKeyUp(KeyCode.Keypad2))
         {
             Debug.Log("down.");
-            switch(lastSelectedButton.name){
-                case "Button_Kommunikation":
-                    changeButton(Entertainment_Button);
-                    break;
-                case "Button_Unterhaltung":
-                    changeButton(Training_Button);
-                    break;
-                case "Button_Training":
-                    changeButton(Help_Button);
-                    break;
+            if(currentPanel == 1){
+                switch(lastSelectedButton.name){
+                    case "Communication_Button":
+                        changeButton(Entertainment_Button);
+                        break;
+                    case "Entertainment_Button":
+                        changeButton(Training_Button);
+                        break;
+                    case "Training_Button":
+                        changeButton(Help_Button);
+                        break;
+                }
+            }else if(currentPanel == 2){
+                switch(lastSelectedButton.name){
+                    case "LeaveHelp_Button":
+                        changeButton(Toilet_Button);
+                        break;
+                    case "Toilet_Button":
+                        changeButton(Pain_Button);
+                        break;
+                    case "Pain_Button":
+                        changeButton(OtherMed_Button);
+                        break;
+                }
             }
         }
     }
